e56da6005ec183bf55fcfe3f90d30b63b1490f5b
EC2 Default User <ec2-user@ip-172-31-58-104.ec2.internal>
2022-06-08 19:29:19 +0000

    controllerファイルの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+  
+  def index
+    @users = User.all
+    @user = User.find(current_user.id)
+  end
+  
+  def show
+    @user =　User.find(params[:id])
+    @user_books = Book.where(user_id: params[:id])
+  end
+
+  def edit
+  end
 end
