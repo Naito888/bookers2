662a82f4f5ad45d3fe9656a280e0ad75648bf726
EC2 Default User <ec2-user@ip-172-31-58-104.ec2.internal>
2022-06-15 13:19:54 +0000

    bookテーブルの作り直し

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,25 +1,29 @@
 class BooksController < ApplicationController
   
-  def new
+  def index
+    @books = Book.all
     @book = Book.new
+    @user = current_user
   end
   
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save!
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      @user = current_user
+      render :index
+    end
   end
-  
+      
   def show
     @book = Book.find(params[:id])
-    @user = @book.user
+    @user = @book.user_id
   end
   
-  
-  def index
-    
-  end
   
   def edit
     
@@ -30,6 +34,5 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
   
 end
